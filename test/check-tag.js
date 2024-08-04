14cedd4f4fecf0d0e0c5335f5ce366d0c2b394dc
sg <sg@iboxindia.com>
2024-08-04 09:43:55 +0530

    debug log

--- test/check-tag.js
+++ test/check-tag.js
@@ -3,12 +3,12 @@ const core = require('@actions/core');
 const { execSync } = require('child_process');
 const packageJson = require('../package.json');
 
+const TAG = packageJson.version;
 try {
-  const TAG = packageJson.version;
   execSync(`git show-ref --tags --verify --quiet refs/tags/v${TAG}`);
   console.log(`Tag v${TAG} already exists.`);
   core.setFailed(`Tag v${TAG} exists. Kindly update the version.`);
   process.exit(1);
 } catch (error) {
-  console.log('Tag does not exist, proceeding with tests...');
+  console.log(`Tag v${TAG} does not exist, proceeding with tests...`);
 }
